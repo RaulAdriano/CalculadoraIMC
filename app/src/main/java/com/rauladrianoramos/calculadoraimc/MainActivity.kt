084944ea5b68df81694d7ba9d27c82d508d2ac3f
Raul Adriano Ramos <raul.ramos@spassu.com.br>
2024-04-17 23:30:37 -0300

    app completo

--- app/src/main/java/com/rauladrianoramos/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/rauladrianoramos/calculadoraimc/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.rauladrianoramos.calculadoraimc
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonCalcular : Button
+    private lateinit var editPeso : EditText
+    private lateinit var editAltura : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        buttonCalcular = findViewById(R.id.button_calcular)
+        editPeso = findViewById(R.id.edit_peso)
+        editAltura = findViewById(R.id.edit_altura)
+
+        buttonCalcular.setOnClickListener {
+
+            val intent = Intent(this, ResultadoActivity::class.java)
+
+            val peso = editPeso.text.toString()
+            val altura = editAltura.text.toString()
+
+
+            if(peso.isNotEmpty() && altura.isNotEmpty()){
+
+                intent.putExtra("peso", peso.toDouble())
+                intent.putExtra("altura", altura.toDouble())
+            }
+
+
+            startActivity(intent)
         }
     }
 }
